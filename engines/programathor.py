094722a731dbfd24441e766095daeee1a1db53a6
Lucelho Cristiano Vieira da Silva <lucelhocristiano28@gmail.com>
2023-11-30 10:16:02 -0300

    fix : correction in job search Co-authored-by: TMMichaelsen <herrtmm@proton.me>

--- engines/programathor.py
+++ engines/programathor.py
@@ -1,51 +1,64 @@
 import httpx
 from bs4 import BeautifulSoup
 
+
 def check_none(value) -> str:
     '''
-    DOCSTRING
+    Checks whether the value is None and, if not, returns the stripped text of the value.
     '''
-    
+
     if value == None:
         return 'Não informado'
     else:
         return value.get_text(strip=True)
 
+
 async def get_programathor_jobs() -> list:
     '''
-    DOCSTRING
+    Asynchronous function that returns a list of lists with the following structure:
+
+    [[code, title, company, location, stack, link], [...], [...], ...]
+
+    Each list within the returned list represents a job vacancy published in the ProgramaThor website.
     '''
 
     base_url = 'https://programathor.com.br'
     jobs = []
 
-    for page in range(1,21):
+    for page in range(1, 21):
         async with httpx.AsyncClient() as client:
             response = await client.get(f'{base_url}/jobs/page/{page}')
             soup = BeautifulSoup(response.content, 'html.parser')
-            
+
             cells = soup.find_all('div', class_='cell-list')
             for cell in cells:
                 title = cell.find('h3')
-
+                # Checks if the cell is blank or if the job is expired
                 if title == None or title.text.startswith('Vencida'):
                     continue
 
-                code = int(''.join(cell.find('a')['href'].split('/jobs/')[1].split('-')[0]))    # Utilizado para verificar repetição de envio do bot no Discord
+                # Used to verify if the job was already sent by the Discord bot
+                code = int(''.join(cell.find('a')['href'].split(
+                    '/jobs/')[1].split('-')[0]))
                 title = title.get_text(strip=True)
-                company = check_none(cell.find('span'))
-                location = check_none(cell.find('span').find_next_sibling())
+                if title.endswith('NOVA'):
+                    # Removes the 'NOVA' string from the title when it's a new job publishing
+                    title = title[:-4]
+
+                details = cell.find('div', class_='cell-list-content-icon')
+                company = check_none(details.find('span'))
+                location = check_none(details.find('span').find_next_sibling())
 
                 all_stacks = cell.select('.tag-list')
                 stack = []
                 for stacks in all_stacks:
                     stacks = check_none(stacks)
                     stack.append(stacks)
-                    
+
                 link = base_url + cell.find('a')['href']
 
                 job = [code, title, company, location, stack, link]
 
                 jobs.append(job)
-        
+
     return jobs
