fdad9d5f8ab256e886a56ca050d50598aaffd5b2
Lucelho Cristiano Vieira Da Silva <lucelhocristiano28@gmail.com>
2023-11-30 14:13:23 -0300

    testes

---
+++ test_bot.py
@@ -0,0 +1,17 @@
+import unittest
+from seu_arquivo_principal import get_html, get_job_info
+
+
+class TestBotMethods(unittest.TestCase):
+
+    def test_get_html(self):
+        # Adicione seus casos de teste para get_html aqui
+        pass
+
+    def test_get_job_info(self):
+        # Adicione seus casos de teste para get_job_info aqui
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
